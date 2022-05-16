150cc09f3bbb2195c58fa6ab9be8e4122a72da93
fjx <1060422838@qq.com>
2022-05-16 18:10:55 +0800

    初始化了三个Activity，设置了MainActivity到另外两个的跳转

--- app/src/main/java/com/example/personallibrary/MainActivity.java
+++ app/src/main/java/com/example/personallibrary/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.personallibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_add = findViewById(R.id.button_add);
+        Button button_check = findViewById(R.id.button_check);
+        button_add.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddBookActivity.class);
+            startActivity(intent);
+        });
+        button_check.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, BookListActivity.class);
+            startActivity(intent);
+        });
     }
 }
